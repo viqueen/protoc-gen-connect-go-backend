f73fb9509b43f5a98634b7e6c3073fa3cf1e133a
viqueen <hasnae.rehioui@safetyculture.io>
2025-01-10 23:06:01 +1100

    welcome connect server

---
+++ internal/codegen/connect_server_file.go
@@ -0,0 +1,126 @@
+package codegen
+
+import (
+	"bytes"
+	"fmt"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"strings"
+	"text/template"
+)
+
+type ConnectServerFileInput struct {
+	DataGenPackage  string
+	PackageName     string
+	InternalPackage string
+	ApiPackage      string
+}
+
+func ConnectServerFile(input ConnectServerFileInput, services []*descriptorpb.ServiceDescriptorProto) (string, error) {
+	tmpl, err := template.New("connectServerFile").Parse(connectServerFileTemplate)
+	if err != nil {
+		return "", err
+	}
+	params := extractConnectServerFileParams(input, services)
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, params)
+	if err != nil {
+		panic(err)
+	}
+	return buf.String(), nil
+}
+
+type connectServerFileParams struct {
+	Services       []serviceDef
+	DataGenPackage string
+}
+
+type serviceDef struct {
+	ServiceNameLower            string
+	ServiceInternalPackage      string
+	ServiceInternalPackageAlias string
+	ServiceName                 string
+	ServiceConnectPackage       string
+	ServiceConnectPackageAlias  string
+}
+
+func extractConnectServerFileParams(input ConnectServerFileInput, services []*descriptorpb.ServiceDescriptorProto) connectServerFileParams {
+	var serviceDefs []serviceDef
+	for _, service := range services {
+		servicePackage := fmt.Sprintf("%s/%s", input.ApiPackage, strings.Replace(input.PackageName, ".", "/", -1))
+		serviceConnectPackageAlias := fmt.Sprintf("%sconnect", toGoAlias(input.PackageName))
+		serviceConnectPackage := fmt.Sprintf("%s/%s", servicePackage, serviceConnectPackageAlias)
+		serviceDefs = append(serviceDefs, serviceDef{
+			ServiceNameLower:            strings.TrimSuffix(strings.ToLower(service.GetName()), "service"),
+			ServiceName:                 strings.TrimSuffix(service.GetName(), "Service"),
+			ServiceInternalPackage:      fmt.Sprintf("%s/api-%s", input.InternalPackage, strings.ReplaceAll(input.PackageName, ".", "-")),
+			ServiceInternalPackageAlias: strings.ReplaceAll(input.PackageName, ".", ""),
+			ServiceConnectPackage:       serviceConnectPackage,
+			ServiceConnectPackageAlias:  serviceConnectPackageAlias,
+		})
+	}
+	return connectServerFileParams{
+		Services:       serviceDefs,
+		DataGenPackage: input.DataGenPackage,
+	}
+}
+
+var connectServerFileTemplate = `
+package main
+
+import (
+	connectcors "connectrpc.com/cors"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/rs/cors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"log"
+	"net/http"
+	gendb "{{.DataGenPackage}}"
+	{{range .Services}}
+	{{.ServiceInternalPackageAlias}} "{{.ServiceInternalPackage}}"
+	{{.ServiceConnectPackageAlias}} "{{.ServiceConnectPackage}}"
+	{{end}}
+)
+
+func main() {
+	db, dbErr := initialiseDB()
+	if dbErr != nil {
+		log.Fatalf("failed to initialise db: %v", dbErr)
+	}
+	dataStore := gendb.New(db)
+
+	mux := http.NewServeMux()
+	{{range .Services}}
+	{{.ServiceNameLower}}Service := {{.ServiceInternalPackageAlias}}.New{{.ServiceName}}Service({{.ServiceInternalPackageAlias}}.{{.ServiceName}}ServiceConfig{Store: dataStore})
+	{{.ServiceNameLower}}Path, {{.ServiceNameLower}}Handler := {{.ServiceConnectPackageAlias}}.New{{.ServiceName}}ServiceHandler({{.ServiceNameLower}}Service)
+	mux.Handle({{.ServiceNameLower}}Path, {{.ServiceNameLower}}Handler)
+	{{end}}
+
+	h2cMux := h2c.NewHandler(mux, &http2.Server{})
+	log.Printf("starting server on :8080")
+	err := http.ListenAndServe(":8080", withCORS(h2cMux))
+	if err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
+
+func initialiseDB() (*sql.DB, error) {
+	connectionString := "postgres://canopy:canopy@localhost:5432/canopy?sslmode=disable"
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func withCORS(h http.Handler) http.Handler {
+	middleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: connectcors.AllowedMethods(),
+		AllowedHeaders: connectcors.AllowedHeaders(),
+		ExposedHeaders: connectcors.ExposedHeaders(),
+	})
+	return middleware.Handler(h)
+}
+`
--- internal/handler/connect_backend.go
+++ internal/handler/connect_backend.go
@@ -1,10 +1,50 @@
 package handler
 
 import (
+	"errors"
+	"github.com/viqueen/go-protoc-gen-plugin/internal/codegen"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/pluginpb"
+	"path/filepath"
 )
 
 func connectBackend(params map[string]string, protoFile *descriptorpb.FileDescriptorProto, response *pluginpb.CodeGeneratorResponse) error {
+	services := protoFile.GetService()
+	if len(services) == 0 {
+		return nil
+	}
+
+	apiPackage, ok := params["api_package"]
+	if !ok {
+		return errors.New("api_package is required")
+	}
+	dataGenPackage, ok := params["data_gen_package"]
+	if !ok {
+		return errors.New("data_gen_package is required")
+	}
+	internalPackage, ok := params["internal_package"]
+	if !ok {
+		return errors.New("internal_package is required")
+	}
+
+	packageName := protoFile.GetPackage()
+
+	serverFileName := "main.go"
+	serverFilePath := filepath.Join("cmd", "server", serverFileName)
+	connectServerFileContent, err := codegen.ConnectServerFile(codegen.ConnectServerFileInput{
+		DataGenPackage:  dataGenPackage,
+		PackageName:     packageName,
+		InternalPackage: internalPackage,
+		ApiPackage:      apiPackage,
+	}, services)
+	if err != nil {
+		response.Error = proto.String(err.Error())
+	}
+	response.File = append(response.File, &pluginpb.CodeGeneratorResponse_File{
+		Name:    proto.String(serverFilePath),
+		Content: proto.String(connectServerFileContent),
+	})
+
 	return nil
 }
