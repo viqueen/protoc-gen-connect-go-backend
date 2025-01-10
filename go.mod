28310d711015ba7706d480f0dec7c3928d713505
viqueen <hasnae.rehioui@safetyculture.io>
2025-01-10 22:27:55 +1100

    increase test coverage

--- go.mod
+++ go.mod
@@ -2,6 +2,16 @@ module github.com/viqueen/go-protoc-gen-plugin
 
 go 1.23.1
 
-require google.golang.org/protobuf v1.36.2
+require (
+	github.com/bufbuild/protocompile v0.14.1
+	github.com/stretchr/testify v1.9.0
+	google.golang.org/protobuf v1.36.2
+)
 
-require github.com/viqueen/protoc-gen-sqlc v0.0.1 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/viqueen/protoc-gen-sqlc v0.0.1
+	golang.org/x/sync v0.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/handler/proto_file_handler_test.go
@@ -0,0 +1,45 @@
+package handler_test
+
+import (
+	"context"
+	"github.com/bufbuild/protocompile"
+	"github.com/bufbuild/protocompile/protoutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/viqueen/go-protoc-gen-plugin/internal/handler"
+	"google.golang.org/protobuf/types/pluginpb"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestProtoFileHandler(t *testing.T) {
+	t.Parallel()
+
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+
+	musicProto := "music/v1/music_models.proto"
+	compiler := protocompile.Compiler{
+		Resolver: &protocompile.SourceResolver{
+			ImportPaths: []string{
+				filepath.Join(cwd, "../../test-protos"),
+			},
+		},
+	}
+	descriptors, err := compiler.Compile(context.Background(), musicProto)
+	require.NoError(t, err)
+
+	params := map[string]string{
+		"api_package":      "api",
+		"data_gen_package": "data",
+	}
+
+	response := &pluginpb.CodeGeneratorResponse{}
+	for _, desc := range descriptors {
+		protoDescriptor := protoutil.ProtoFromFileDescriptor(desc)
+		err = handler.ProtoFileHandler(params, protoDescriptor, response)
+		assert.NoError(t, err)
+		assert.Len(t, response.File, 2)
+	}
+}
